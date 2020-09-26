2e75e27cc1b87a9e79155f14bd2719ad2d865431
Remias <remias.david5761@gmail.com>
2020-09-26 11:12:35 +0300

    Method for checking board validity.

--- threeInARowGame/src/main/java/org/example/game/GameUtils.java
+++ threeInARowGame/src/main/java/org/example/game/GameUtils.java
@@ -1,59 +1,29 @@
 package org.example.game;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+package org.example.game;
+
 
 public class GameUtils {
 
     public static boolean isValidBoard(int[][] board){
-        return true;
+        if(board == null || board.length != 5){
+            return false;
+        }
+        int countRed=0;
+        int countBlue=0;
+        for(int[] row:board){
+            if(row == null || row.length !=4){
+                return false;
+            }
+            for(int a:row){
+                if(a<0 || a>Piece.values().length){
+                    return false;
+                }
+                countBlue += (a==2)?1:0;
+                countRed += (a==1)?1:0;
+            }
+        }
+        return countBlue == 4 && countRed == 4;
     }
 
-    public static List<Direction> whereToMove(int row, int column, Piece[][] boardData){return new LinkedList<>();}
-    public static boolean canMoveUp(int row, int column, Piece[][] boardData){
-        return true;
-    }
-
-    public static boolean canMoveDown(int row, int column, Piece[][] boardData){
-        return true;
-    }
-
-    public static boolean canMoveRight(int row, int column,Piece[][] boardData){
-        return true;
-    }
-
-    public static boolean canMoveLeft(int row, int column,Piece[][] boardData){
-        return true;
-    }
-
-    /**
-     * Checks whether the game is ended.
-     *
-     * @return {@code true} if one of the players has 3 piece in a row, column or diagonal,
-     * {@code false} otherwise
-     */
-    public static boolean isEnd(Piece[][] boardData) {
-        return true;
-    }
-
-    public static boolean isThreeInARow(Piece[][] boardData){
-        return true;
-    }
-
-    public static boolean isThreeInAColumn(Piece[][] boardData){
-        return true;
-    }
-
-    public static boolean isThreeInDiagonal(Piece[][] boardData){
-        return true;
-    }
-    public static int[][] makeValidBoard(String state){
-        return new int[1][1];
-    }
-
-    public static String createStringFromGameState(Piece[][] boardData){
-        return "";
-    }
-
-}
+  
