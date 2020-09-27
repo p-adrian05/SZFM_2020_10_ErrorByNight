03f0132c3df3b5ef436d74e2f63c3b8970f88f9b
Szemán Dávid <szeman.david@gmail.com>
2020-09-27 17:26:16 +0200

    Added test method for testing getWinnerName method in GameServiceImpl class

--- threeInARowGame/src/test/java/game/GameServiceImplTest.java
+++ threeInARowGame/src/test/java/game/GameServiceImplTest.java
@@ -4,8 +4,18 @@ import org.example.gamedb.GameState;
 import org.example.gamedb.GameStateDao;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceImplTest {
 
+    GameService gameService = GameServiceImpl.getInstance();
+
+    @Test
+    void getWinnerName() {
+        gameService.reset();
+        assertEquals(Piece.RED.name(),gameService.getWinnerName());
+    }
 }
