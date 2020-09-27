cbf664d37dd93319c81a586eb27efd92cdafb867
Szemán Dávid <szeman.david@gmail.com>
2020-09-27 17:15:42 +0200

    Added GameServiceImplTest class for testing GameServiceImpl class

---
+++ threeInARowGame/src/test/java/game/GameServiceImplTest.java
@@ -0,0 +1,11 @@
+package org.example.game;
+
+import org.example.gamedb.GameState;
+import org.example.gamedb.GameStateDao;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceImplTest {
+
+}
