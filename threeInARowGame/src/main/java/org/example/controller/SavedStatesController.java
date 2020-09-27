68e1f35c8b20e607806fa8e1728fef25c2233b62
Mátravölgyi Adrián <43448141+matravolgyiadrian@users.noreply.github.com>
2020-09-27 20:38:56 +0200

    Update SavedStatesController.java
    Added displayList() function to display the saved games

--- threeInARowGame/src/main/java/org/example/controller/SavedStatesController.java
+++ threeInARowGame/src/main/java/org/example/controller/SavedStatesController.java
@@ -1,13 +1,22 @@
 package org.example.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.example.game.GameService;
+import org.example.game.GameServiceImpl;
 import org.example.gamedb.GameState;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
 
 
 @Slf4j
@@ -26,6 +35,8 @@ public class SavedStatesController {
     @FXML
     private Button loadButton;
 
+    private final GameService gameService = GameServiceImpl.getInstance();
+    
     @FXML
     private void initialize(){
         log.debug("Loading saved games...");
@@ -33,6 +44,33 @@ public class SavedStatesController {
     }
 
     private void displayList(){
+        List<GameState> savedGames = gameService.getAllSavedGameStates();
+
+        redPlayer.setCellValueFactory(new PropertyValueFactory<>("redPlayerName"));
+        bluePlayer.setCellValueFactory(new PropertyValueFactory<>("bluePlayerName"));
+        created.setCellValueFactory(new PropertyValueFactory<>("created"));
+
+        created.setCellFactory(column -> {
+            TableCell<GameState, ZonedDateTime> cell = new TableCell<>() {
+                private final DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
+
+                @Override
+                protected void updateItem(ZonedDateTime item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(item.format(formatter));
+                    }
+                }
+            };
+            return cell;
+        });
+
+        ObservableList<GameState> observableList = FXCollections.observableArrayList();
+        observableList.addAll(savedGames);
+
+        savedGamesTable.setItems(observableList);
     }
 
     @FXML
