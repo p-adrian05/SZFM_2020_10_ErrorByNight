2c08d1d34c6d549874e513a116baddd41d78eaf8
p-adrian05 <adrian16616@gmail.com>
2020-09-27 12:09:28 +0200

    Add test method  for testing of method in Direction enum class

--- threeInARowGame/src/test/java/game/DirectionTest.java
+++ threeInARowGame/src/test/java/game/DirectionTest.java
@@ -1,5 +1,6 @@
 package game;
 
+import org.example.game.Direction;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +9,10 @@ class DirectionTest {
 
     @Test
     void of() {
+        assertEquals(Direction.UP, Direction.of(-1, 0));
+        assertEquals(Direction.DOWN, Direction.of(1, 0));
+        assertEquals(Direction.RIGHT, Direction.of(0, 1));
+        assertEquals(Direction.LEFT, Direction.of(0, -1));
+        assertThrows(IllegalArgumentException.class, () -> Direction.of(1, 1));
     }
 }
