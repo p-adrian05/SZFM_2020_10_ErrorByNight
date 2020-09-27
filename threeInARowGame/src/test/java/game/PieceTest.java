ec3c2cc5c70d79d1a511ca830e6f18938324b005
p-adrian05 <adrian16616@gmail.com>
2020-09-27 12:10:58 +0200

    Add test method  for testing of method in  Piece enum class

--- threeInARowGame/src/test/java/game/PieceTest.java
+++ threeInARowGame/src/test/java/game/PieceTest.java
@@ -1,11 +1,17 @@
 package game;
 
+import org.example.game.Piece;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PieceTest {
 
     @Test
     void of() {
-
+        assertThrows(IllegalArgumentException.class, () -> Piece.of(-1));
+        assertEquals(Piece.EMPTY, Piece.of(0));
+        assertEquals(Piece.RED, Piece.of(1));
+        assertEquals(Piece.BLUE, Piece.of(2));
+        assertThrows(IllegalArgumentException.class, () -> Piece.of(3));
     }
 }
