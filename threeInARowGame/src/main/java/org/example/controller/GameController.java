ca63b9ffa08d5bf563233f4386e7b8729b919d89
p-adrian05 <adrian16616@gmail.com>
2020-09-27 15:35:41 +0200

    Made handling clicks on game board in GameController class

--- threeInARowGame/src/main/java/org/example/controller/GameController.java
+++ threeInARowGame/src/main/java/org/example/controller/GameController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,6 +11,11 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import lombok.extern.slf4j.Slf4j;
+import org.example.game.Direction;
+import org.example.game.GameService;
+import org.example.game.GameServiceImpl;
+
+import java.util.List;
 
 
 @Slf4j
@@ -32,12 +38,43 @@ public class GameController {
     private Button continueButton;
     @FXML
     private Label errorLabel;
+
+    private final GameService gameService = GameServiceImpl.getInstance();
+    private List<Direction> possibleMoves;
+    private boolean isHighlighted = false;
+    private int clickedRow, clickedColumn;
+
     @FXML
     public void initialize() {
         displayGameState();
     }
     @FXML
     public void handleMovement(MouseEvent mouseEvent){
+        if(!gameService.gameOver()){
+            handleClicks(mouseEvent);
+        } else{
+            handleGameEnding();
+        }
+    }
+    private void handleClicks(MouseEvent mouseEvent){
+        if(isHighlighted) {
+            handleClickIfIsHighlighted(mouseEvent);
+        } else{
+            highlightLegalMoves(mouseEvent);
+        }
+    }
+
+    private void handleClickIfIsHighlighted(MouseEvent mouseEvent){
+        Node source = (Node) mouseEvent.getSource();
+        int row = GridPane.getRowIndex(source);
+        int column = GridPane.getColumnIndex(source);
+        for(Direction direction:possibleMoves){
+            if(row == clickedRow+direction.getDx() && column == clickedColumn+direction.getDy()){
+                move(direction);
+                break;
+            }
+            eraseHighlight();
+        }
     }
 
     private void displayGameState() {
