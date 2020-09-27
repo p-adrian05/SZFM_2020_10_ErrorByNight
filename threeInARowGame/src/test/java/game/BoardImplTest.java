34de7347a5a54c80e139c7db7dead14bc12e0008
p-adrian05 <adrian16616@gmail.com>
2020-09-27 12:02:38 +0200

    Add test for initialize board in BoradImplTest class

--- threeInARowGame/src/test/java/game/BoardImplTest.java
+++ threeInARowGame/src/test/java/game/BoardImplTest.java
@@ -1,10 +1,46 @@
 package game;
 
+import org.example.game.BoardImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardImplTest {
+
+    @Test
+    void initBoard() {
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(null));
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(new int[][]{
+                {1, 2},
+                {2, 0}})
+        );
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(new int[][]{
+                {0, 0, 0, 1},
+                {1, 2, 0, 2},
+                {0, 0, 1, 0}})
+        );
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(new int[][]{
+                {0, 0, 0, 0},
+                {1, 1, 0, 2},
+                {0, 1, 2, 0},
+                {1, 0, 0, 0},
+                {0, 0, 0, 0}})
+        );
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(new int[][]{
+                {0, 0, 0, 0},
+                {1, 1, 0, 2},
+                {0, 1, 2, 0},
+                {1, 0, 2, 0},
+                {0, 3, 0, 2}})
+        );
+        assertThrows(IllegalArgumentException.class, () -> new BoardImpl(new int[][]{
+                {0, 0, 1, 0},
+                {1, 0, 0, 2},
+                {0, 2, 2, 0},
+                {1, 0, 0, 0},
+                {0, 0, 0, 0}})
+        );
+    }
 
     @Test
     void nextTurn() {
@@ -14,11 +50,5 @@ class BoardImplTest {
     void swapPieces() {
     }
 
-    @Test
-    void getBoardState() {
-    }
 
-    @Test
-    void getTurn() {
-    }
 }
