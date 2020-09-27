f68cb51d18962cc5b129d05a480ba4b33b6549b6
p-adrian05 <adrian16616@gmail.com>
2020-09-27 12:01:07 +0200

    Made test folder for test classes and made BoradImplTest class

---
+++ threeInARowGame/src/test/java/game/BoardImplTest.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardImplTest {
+
+    @Test
+    void nextTurn() {
+    }
+
+    @Test
+    void swapPieces() {
+    }
+
+    @Test
+    void getBoardState() {
+    }
+
+    @Test
+    void getTurn() {
+    }
+}
